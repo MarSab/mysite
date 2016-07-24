e12838b145aa9cfb19f37e63951fd04c2f4de76b
Mariella Sabatino <mll.sabatino@gmail.com>
2016-07-24 17:05:12 +0200

    first commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('mysite')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+
+//app constructor: appname, dependecies array
+  angular
+    .module('mysite', ['ngSanitize', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
